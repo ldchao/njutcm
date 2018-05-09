1e014810ee1698bce398ec4375c2373bffb0ffe1
ldchao <379905230@qq.com>
2018-05-09 21:08:51 +0800

    创建项目

---
+++ RNATranscription/src/main/java/cn/edu/nju/njutcm/rna/RnaApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.njutcm.rna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnaApplication.class, args);
+	}
+}
---
+++ RNATranscription/src/test/java/cn/edu/nju/njutcm/rna/RnaApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.nju.njutcm.rna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RnaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
